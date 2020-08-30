d81358ebdaff6840846f4e38f691422f65e86204
Josh Bleecher Snyder <josharian@gmail.com>
2020-08-30 15:20:01 -0700

    fix spelling mistake in example

--- example_test.go
+++ example_test.go
@@ -7,13 +7,13 @@ import (
 )
 
 func ExampleCommand() {
-	cmd := loggedexec.Command("ls", "/tmp/nonexistant")
+	cmd := loggedexec.Command("ls", "/tmp/nonexistent")
 	cmd.Env = []string{"LANG=C"}
 	if err := cmd.Run(); err != nil {
 		fmt.Println(err)
 	}
-	// Output: Running "ls /tmp/nonexistant": exit status 2
+	// Output: Running "ls /tmp/nonexistent": exit status 2
 	// See "/tmp/000-ls.invocation.log" for invocation details.
 	// See "/tmp/000-ls.stdoutstderr.log" for full stdout/stderr.
-	// First stdout/stderr line: "ls: cannot access /tmp/nonexistant: No such file or directory"
+	// First stdout/stderr line: "ls: cannot access /tmp/nonexistent: No such file or directory"
 }
